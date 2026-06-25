fb8de9a4052b66d30e6df3693cf0d8116d0b92e1
varshinicb1 <cbvarshini1@gmail.com>
2026-06-25 06:44:50 +0530

    test: add fake API key to verify PR scan workflow

---
+++ test-leaked-key.js
@@ -0,0 +1,3 @@
+// Test file for PR scan workflow verification
+// This contains a fake API key to test that env-secure detects it
+const stripeKey = 'SG.abcdefghijklmnopqrstuvwxyz.abcdefghijklmnopqrstuvwxyzABCDEFGHIJ';
